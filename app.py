54c2f27197b171d48b2c4bd92071d2ecad5d0e03
denver <denver@projectcodex.co>
2015-07-13 14:34:47 +0200

    updates

--- app.py
+++ app.py
@@ -13,24 +13,36 @@ app.config['MYSQL_DB'] = 'my_products'
 
 
 
-"""names of products"""
+
 @app.route('/')
 def prodList():
 	cur = mysql.connection.cursor()
 	cur.execute('''SELECT Name from Products''')
 	entries = [dict(Name=row[0]) for row in cur.fetchall()]
-    	return render_template('menu.html', entries=entries)
+	print entries
+
+	cur.execute('''SELECT SUM(Qty) AS TotalQty , Product_Id, Name from Sales s INNER JOIN Products p ON s.Product_Id = p.Id 					GROUP BY Name''')
+	products = [dict(Name=row[2],Qty=row[0]) for row in cur.fetchall()]
+	
+        cur.execute('''SELECT Name from Categories''')
+	categories = [dict(Name=row[0]) for row in cur.fetchall()]
+       
+        cur.execute('''SELECT  Categories.Name, sum(Sales.Qty) AS TotalQty from Sales INNER JOIN Products ON Sales.Product_id = Products.Id INNER JOIN Categories ON Products.Category_id = Categories.Id GROUP BY Categories.Name''')
+	cats = [dict(Name=row[0],Qty=row[1]) for row in cur.fetchall()]
+	
+    	return render_template('menu.html',  entries=entries, products=products, categories=categories, cats=cats)
 
 
 """product list with qty sold"""
-@app.route('/')
+"""
+@app.route('/dee')
 def products():
 	
 	cur.execute('''SELECT SUM(Qty) AS TotalQty , Product_Id, Name from Sales s INNER JOIN Products p ON s.Product_Id = p.Id 					GROUP BY Name''')
 	products = [dict(Name=row[2],Qty=row[0]) for row in cur.fetchall()]
-    	return render_template('menu.html', products=products)
+    	return render_template('dee.html', products=products)
 
-"""category list with qty sold"""
+
 @app.route('/categorySales')
 def categories():
 	cur = mysql.connection.cursor()
@@ -38,13 +50,19 @@ def categories():
 	entries = [dict(Name=row[0],Qty=row[1]) for row in cur.fetchall()]
     	return render_template('categories.html', entries=entries)
 
-"""names of categories"""
+
 @app.route('/catList')
 def catList():
 	cur = mysql.connection.cursor()
 	cur.execute('''SELECT Name from Categories''')
 	entries = [dict(Name=row[0]) for row in cur.fetchall()]
     	return render_template('catList.html', entries=entries)
+
+@app.route('/delete', methods=['DELETE'])
+def delete_entry(postID):
+    g.db.execute('delete from entries WHERE Id = ?', [postID])
+    flash('Entry was deleted')
+    return redirect(url_for('/'))""" 
 
 
 if __name__ == '__main__':
